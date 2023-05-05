1e4c47da9d6d0fd5525d708d8b0f2cc62d4747f8
PabloSanturio <pablosanturio1@gmail.com>
2023-05-05 14:28:29 -0300

    Importacion de las fuentes~

--- src/Layouts/Init/Init.jsx
+++ src/Layouts/Init/Init.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import "./Init.css";
 function Init() {
-    return (
-        <div>Init
-
-        </div>
-    )
+  return (
+    <div className="init-container">
+      <div>
+        <p>Música a medida.</p>
+      </div>
+      <div>
+        <button className="btn-standardd">Registrarse Gratis</button>
+        <button>Continuar con Google</button>
+        <button>Continuar con Apple</button>
+        <button>Iniciar Sesión</button>
+      </div>
+    </div>
+  );
 }
 
-export default Init
+export default Init;
