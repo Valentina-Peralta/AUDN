60286fa06a28ed6f42967d5b16a8593cd7cd19ce
Noegit1582 <quererespodernrm@gmail.com>
2023-05-09 11:47:48 -0300

    layout pl cupido

--- src/Layouts/Playlist/Playlist.jsx
+++ src/Layouts/Playlist/Playlist.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Playlist() {
     return (
-        <div>Playlist</div>
+        <div>
+                    <div className='plContainer'>
+            <div className='plTitle'>
+                <NavLink to='/home'>
+                    <img src="src\Img\position=left-1.svg" alt="backArrow" />
+                </NavLink>
+                <h5>Generada del Cupido Musical</h5>
+                <h3 className='pl'>Playlist Generadas</h3>
+                <img src="src\Img\position=vertical.svg" alt="" />
+
+            </div>
+        </div>
+        </div>
     )
 }
 
