ed9651117c3bc11132e92408f7b5264d882500b7
Valentina-Peralta <125395224+Valentina-Peralta@users.noreply.github.com>
2023-05-09 08:30:19 -0300

    layout profile

---
+++ src/Layouts/Profile/Config.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Config() {
+    return (
+        <div>
+            <div className="top-gradient" />
+            <div className="top-nav-bar">
+                <p>Configuración</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Config
